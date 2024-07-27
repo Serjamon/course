8d139aec994b23ecd6dda76024298f8bc1e8c760
Sergey Akimov <rsl2folly@gmail.com>
2024-07-27 17:38:05 +0300

    task #1 "Tochka"

--- src/main/java/MyProg.java
+++ src/main/java/MyProg.java
@@ -1,12 +1,19 @@
 public class MyProg {
     public static void main(String[] args) {
 
-        A y = new A(5);
-        System.out.println(y);
+        {//Практика ООП. Объекты и классы #1. Точка координат
+            Point tochka1 = new Point(1, 3);
+            Point tochka2 = new Point(1, 3);
+            Point tochka3 = new Point(5, 8);
+            System.out.println(tochka1);
+            System.out.println(tochka2);
+            System.out.println(tochka3);
+            System.out.println(tochka1 == tochka2);
+            System.out.println(tochka2 == tochka3);
+        }
 
-        A sss = new A("12");
-        y.xyz();
-        sss.xyz();
+
+
 
     }
 }
