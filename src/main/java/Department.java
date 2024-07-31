59ccbf8fc4583b6558956f8bd8977c0b9f157342
Sergey Akimov <rsl2folly@gmail.com>
2024-07-31 19:20:48 +0300

    task #4 Employee

---
+++ src/main/java/Department.java
@@ -0,0 +1,25 @@
+public class Department {
+
+    String deptName;
+    private Employee boss;
+
+    public Department(String deptName) {
+        this.deptName = deptName;
+        //при создании отдела в нем никто не работает, начальника быть не может
+    }
+
+    public void setBoss(Employee boss) {
+        if (boss.worksInDept(this)) this.boss = boss;
+        else System.out.println("Нельзя назначить начальником сотрудника другого отдела!");
+    }
+
+    public String getBossName() {
+        return (boss == null ? "пока не назначен" : boss.name);
+    }
+
+    public boolean emplIsBoss(Employee empl) {
+        return empl == boss;
+    }
+
+
+}
--- src/main/java/MyProg.java
+++ src/main/java/MyProg.java
@@ -3,9 +3,23 @@
 public class MyProg {
     public static void main(String[] args) {
 
-        Square sq1 = new Square(2,3, 1);
-        System.out.println(sq1);
-        sq1.setSideLength(-5);
+        Department dAccounting = new Department("Отдел бухгалтерии");
+        Department dIT = new Department("ИТ отдел");
+        Department dCleaning = new Department("Отдел обслуживания");
+
+        Employee emp1 = new Employee("Иван Васильевич", dAccounting);
+        Employee emp2 = new Employee("Вася", dIT);
+        Employee emp3 = new Employee("Иван Федорович", dCleaning);
+        Employee emp4 = new Employee("Елена Степановна", dCleaning);
+
+        System.out.println(dIT.getBossName());
+        System.out.println(emp2);
+        dIT.setBoss(emp1);
+        dIT.setBoss(emp2);
+        System.out.println(dIT.getBossName());
+        System.out.println(emp1);
+        System.out.println(emp2);
+        emp2.setDept(dCleaning);
 
     }
 }
