487c57d000d609623d9d9bff349fab80a6057f78
Sergey Akimov <rsl2folly@gmail.com>
2024-07-31 19:20:48 +0300

    refactoring

---
+++ src/main/java/ru/courses/organization/Employee.java
@@ -0,0 +1,29 @@
+package ru.courses.organization;
+
+public class Employee {
+
+    String name; //имя может меняться
+    private Department dept;
+
+    public Employee(String name, Department dept) {
+        this.name = name;
+        this.dept = dept;//вновь созданный сотр не может быть начальником
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+        //не нашел как провести проверку на это:
+        //System.out.println("Нельзя перевести начальника в другой отдел!");
+    }
+
+    public boolean worksInDept(Department inDep){
+        return inDep == dept;
+    }
+
+    @Override
+    public String toString() {
+        if (dept.emplIsBoss(this)) return name + " начальник отдела " + dept.deptName;
+        return name + " работает в отделе " +
+                dept.deptName + ", начальник которого " + dept.getBossName();
+    }
+}
