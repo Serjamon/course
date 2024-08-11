908f80af053f76dac00722709b0e247f517bbd8a
Sergey Akimov <rsl2folly@gmail.com>
2024-08-11 15:04:25 +0300

    Packages #3: myPow

--- src/main/java/ru/courses/main/MyProg.java
+++ src/main/java/ru/courses/main/MyProg.java
@@ -1,10 +1,26 @@
 package ru.courses.main;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
+
+
 public class MyProg {
     public static void main(String[] args) {
 
-        System.out.println("test");
+        System.out.println(myPow(args));
 
     }
+
+    public static double myPow(String... x) {
+
+        if (x.length != 2) return 0;
+
+        double intX = parseInt(x[0]);
+        double intY = parseInt(x[1]);
+
+        return pow(intX, intY);
+
+    }
+
 
 }
