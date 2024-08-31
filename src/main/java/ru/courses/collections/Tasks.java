acd2c61f3c3347644154789b66f0e542f13c6a55
Sergey Akimov <rsl2folly@gmail.com>
2024-08-31 21:11:23 +0300

    Collections #3: reverse

--- src/main/java/ru/courses/collections/Tasks.java
+++ src/main/java/ru/courses/collections/Tasks.java
@@ -2,42 +2,40 @@ package ru.courses.collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Tasks {
     public static void main(String[] args) {
 
-        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(3, 2, 6, 5, 4, 1));
-        bubbleSort(intList);
+        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1, 0));
+        reverse(intList);
 
-        int[] intArr = new int[]{9, 2, 8, 5, 4, 1};
-        bubbleSort(intArr);
+        int[] intArr = new int[]{5, 4, 3, 2, 1, 0};
+        reverse(intArr);
 
     }
 
-    public static void bubbleSort(ArrayList<Integer> intList) {
+    public static void reverse(int[] intArray) {
+        int n = intArray.length - 1;
 
-        for (int i = 0; i < intList.size() - 1; i++) {
-            for (int j = 0; j < intList.size() - i - 1; j++) {
-                if (intList.get(j) > intList.get(j + 1)) {
-                    int temp = intList.get(j);
-                    intList.set(j, intList.get(j + 1));
-                    intList.set(j + 1, temp);
-                }
-            }
+        for (int i = 0; i < intArray.length / 2; i++) {
+
+            int temp = intArray[i];
+            intArray[i] = intArray[n - i];
+            intArray[n - i] = temp;
+
         }
+
+    }
+
+    public static void reverse(ArrayList<Integer> intList) {
+
+        Collections.reverse(intList);
+
         System.out.println(intList);
-    }
-
-    public static void bubbleSort(int[] intArray) {
-
-        ArrayList<Integer> intList = new ArrayList<>();
-
-        for (int i = 0; i < intArray.length; i++) intList.add(intArray[i]);
-
-        bubbleSort(intList);
 
     }
-
-
 }
+
+
