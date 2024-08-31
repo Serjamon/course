8c6136d36627a82c8179f5835743dc0ce8007ec2
Sergey Akimov <rsl2folly@gmail.com>
2024-08-31 20:57:46 +0300

    Collections #2: bubble sort

--- src/main/java/ru/courses/collections/Tasks.java
+++ src/main/java/ru/courses/collections/Tasks.java
@@ -1,21 +1,43 @@
 package ru.courses.collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Tasks {
     public static void main(String[] args) {
-        int n = 10;
-        List<Integer> list = new ArrayList<>();
 
-        for (int i = 1; i <= n; i++) list.add(i);
+        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(3, 2, 6, 5, 4, 1));
+        bubbleSort(intList);
 
-        for (int i = 0; i < list.size() - 1; i += 2) {
-            int tmp = list.get(i);
-            list.set(i, list.get(i + 1));
-            list.set(i + 1, tmp);
-        }
+        int[] intArr = new int[]{9, 2, 8, 5, 4, 1};
+        bubbleSort(intArr);
 
-        System.out.println(list);
     }
+
+    public static void bubbleSort(ArrayList<Integer> intList) {
+
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
+            }
+        }
+        System.out.println(intList);
+    }
+
+    public static void bubbleSort(int[] intArray) {
+
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        for (int i = 0; i < intArray.length; i++) intList.add(intArray[i]);
+
+        bubbleSort(intList);
+
+    }
+
+
 }
